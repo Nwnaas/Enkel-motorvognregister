7d0d736696356d7cc3b3ccf38c2acd5b15cd7982
“Nwnaas” <nwnaas@icloud.com>
2023-12-16 20:25:57 +0000

    Initial commit

---
+++ src/main/java/com/example/enkelmotorvognregister/EnkelMotorvognregisterApplication.java
@@ -0,0 +1,13 @@
+package com.example.enkelmotorvognregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnkelMotorvognregisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnkelMotorvognregisterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/enkelmotorvognregister/Motorvogn.java
@@ -0,0 +1,2 @@
+package com.example.enkelmotorvognregister;public class Motorvogn {
+}
