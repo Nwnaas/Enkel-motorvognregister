7e10b9307db1bb17d1949afcb0f1b5b11bb9742a
“Nwnaas” <nwnaas@icloud.com>
2023-12-20 11:57:37 +0000

    Har gjordt noen endringer på java

--- src/main/java/com/example/enkelmotorvognregister/Motorvogn.java
+++ src/main/java/com/example/enkelmotorvognregister/Motorvogn.java
@@ -1,6 +1,7 @@
 package com.example.enkelmotorvognregister;
 
 public class Motorvogn {
+
     private String personNr;
     private String kjonn;
     private String navn;
@@ -8,14 +9,11 @@ public class Motorvogn {
     private String bilSkilt;
     private String bilMerke;
     private String bilModel;
+    private String bilFarge;
     private String sted;
+    private String fjern;
 
-    // Constructors
-    public Motorvogn() {
-        // Standard konstruktør
-    }
-
-    public Motorvogn(String personNr,String kjonn, String navn, String adresse, String bilSkilt, String bilMerke, String bilModel, String sted) {
+    public Motorvogn(String personNr,String kjonn, String navn, String adresse, String bilSkilt, String bilMerke, String bilModel, String bilFarge, String sted, String fjern) {
         this.personNr = personNr;
         this.kjonn = kjonn;
         this.navn = navn;
@@ -23,6 +21,87 @@ public class Motorvogn {
         this.bilSkilt = bilSkilt;
         this.bilMerke = bilMerke;
         this.bilModel = bilModel;
+        this.bilFarge = bilFarge;
+        this.sted = sted;
+        this.fjern = fjern;
+    }
+
+    public Motorvogn() {
+
+    }
+
+    public String getPersonNr() {
+        return personNr;
+    }
+
+    public void setPersonNr(String personNr) {
+        this.personNr = personNr;
+    }
+
+    public String getKjonn() {
+        return kjonn;
+    }
+
+    public void setKjonn(String kjonn) {
+        this.kjonn = kjonn;
+    }
+
+    public String getNavn() { return navn; }
+
+    public void setNavn(String navn) { this.navn = navn; }
+
+    public String getAdresse() { return adresse; }
+
+    public void setAdresse(String adresse) { this.adresse = adresse; }
+
+    public String getBilSkilt() { return bilSkilt; }
+
+    public void setBilSkilt(String bilSkilt) { this.bilSkilt = bilSkilt; }
+
+    public String getBilMerke() { return bilMerke; }
+
+    public void setBilMerke(String bilMerke) { this.bilMerke = bilMerke; }
+
+    public String getBilModel() { return bilModel; }
+
+    public void setBilModel(String bilModel) { this.bilModel = bilModel; }
+
+    public String getBilFarge() { return bilFarge; }
+
+    public void setBilFarge(String bilFarge) { this.bilFarge = bilFarge; }
+
+    public String getSted() { return sted; }
+
+    public void setSted(String sted) { this.sted = sted; }
+
+    public String getFjern() { return fjern; }
+
+    public void setFjern(String fjern) { this.fjern = fjern; }
+
+    /* private String personNr;
+    private String kjonn;
+    private String navn;
+    private String adresse;
+    private String bilSkilt;
+    private String bilMerke;
+    private String bilModel;
+    private String bilFarge;
+    private String sted;
+
+    // Constructors
+    public Motorvogn() {
+        // Standard konstruktør
+    }
+
+    public Motorvogn(String personNr,String kjonn, String navn, String adresse, String bilSkilt, String bilMerke, String bilModel, String bilFarge, String sted) {
+        this.personNr = personNr;
+        this.kjonn = kjonn;
+        this.navn = navn;
+        this.adresse = adresse;
+        this.bilSkilt = bilSkilt;
+        this.bilMerke = bilMerke;
+        this.bilModel = bilModel;
+        this.bilFarge = bilFarge;
         this.sted = sted;
     }
 
@@ -55,6 +134,9 @@ public class Motorvogn {
         return bilModel;
     }
 
+    public String getBilFarge() {
+        return bilFarge;
+    }
     public String getSted() {
         return sted;
     }
@@ -88,7 +170,12 @@ public class Motorvogn {
         this.bilModel = bilModel;
     }
 
+    public void setBilFarge(String bilFarge) {
+        this.bilFarge = bilFarge;
+    }
     public void setSted(String sted) {
         this.sted = sted;
     }
+
+    */
 }
